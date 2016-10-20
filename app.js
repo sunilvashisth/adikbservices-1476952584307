8f936bb8643460196ed79b1a137d892920de812f
Sunil Vashisth <suvashis@in.ibm.com>
2016-10-20 03:39:48 -0500

    sso

--- app.js
+++ app.js
@@ -26,3 +26,46 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+var https = require('https');
+
+app.get('/kbsso', function(req, res){	
+	//console.log('req JSON header: '+JSON.stringify(req.headers));
+	//console.log('req header: '+req.headers.authorization);
+
+	var options = {
+	  host: 'mobile.us.ibm.com',
+      port: 15014,
+      path: '/services/ppm/mobile/img/portal_logo.png',
+	  method: 'GET',      
+	  headers: {
+		  'Authorization': req.headers.authorization
+	  },
+	  preventCache: true,
+	  sync: true  
+	};
+	
+	var request = https.get(options, function(response) {
+	  console.log('STATUS: ' + response.statusCode);
+	  //status = response.statusCode;
+	  //console.log('REq HEADERS: ' + JSON.stringify(request.headers));
+	  //console.log('HEADERS: ' + JSON.stringify(response.headers));
+	  
+	  response.on('data', function (chunk) {
+		//console.log('BODY: ' + chunk);
+	  });
+	  response.on('end', function() {
+		console.log('No more data in response.');
+		//request.end();
+		res.writeHead(response.statusCode);
+		res.end("Welcome to SIH Knowledge Bite"); 
+	  })
+	  	
+	});
+
+	request.on('error', function(e) {
+	  console.log('Error - problem with request: ' + e.message);
+	});	
+	request.end();
+});
+
